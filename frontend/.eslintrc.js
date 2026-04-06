c4c18c98538578f779b352cf87e8d6768da756b9
Agranova Admin <admin@agranova.com>
2026-04-06 15:04:12 +0400

    done

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,9 +6,9 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'off',
     'import/no-anonymous-default-export': 'off',
     'no-use-before-define': 'off',
+    'react/prop-types': 'off',
     // Keep some useful rules but as warnings
-    'react/jsx-no-undef': 'warn',
-    'react/prop-types': 'warn'
+    'react/jsx-no-undef': 'warn'
   },
   settings: {
     react: {
